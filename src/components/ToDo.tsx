bdc4ce672481858fd187a1dfcd311b7f93acb316
Lucca Tourinho <luccatourinho@gmail.com>
2024-07-17 20:23:49 -0300

    Trocamos Edit e Delete por icons e concertamos o tamanho para telas pequenas

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -32,16 +32,16 @@ const ToDo: React.FunctionComponent<ToDoProps> = ( props ) => {
     <div className='flex flex-row bg-white h-10 w-full p-2 rounded-lg shadow-xl'>
       {!updateItem ? 
           <>
-            <p className='w-[90%] px-2'>{item.ToDo}</p> 
-            <div className='flex justify-between gap-2 py-1/2 w-[10%]'>
+            <p className='w-[80%] lg:w-[90%]  px-2'>{item.ToDo}</p> 
+            <div className='flex justify-between gap-2 py-1/2 w-[20%] lg:w-[10%]'>
               <button 
-                className='flex justify-center w-full bg-blue-400 text-white font-bold rounded-full hover:scale-110 shadow-lg'
+                className='flex justify-center w-full items-center p-1 bg-blue-400 text-white font-bold rounded-full hover:scale-110 shadow-lg'
                 onClick={toggleEdit}
-              >Edit</button>
+              ><i className="fa-solid fa-pen-to-square"></i></button>
               <button
-                className='flex justify-center w-full bg-red-400 text-white font-bold rounded-full hover:scale-110 shadow-lg'
+                className='flex justify-center items-center p-1 w-full bg-red-400 text-white font-bold rounded-full hover:scale-110 shadow-lg'
                 onClick={handleDelete}
-              >Delete</button>
+              ><i className="fa-solid fa-trash"></i></button>
             </div>
           </>
           :
@@ -51,17 +51,17 @@ const ToDo: React.FunctionComponent<ToDoProps> = ( props ) => {
               placeholder='Digit your ToDo Activity here'
               value={item.ToDo}
               onChange={handleChange}
-              className='w-[90%] px-2 cursor-pointer focus:outline-none Play text-slate-500'
+              className='w-[80%] lg:w-[90%] px-2 cursor-pointer focus:outline-none Play text-slate-500'
             />
-            <div className='flex justify-between gap-2 py-1/2 w-[10%]'>
+            <div className='flex justify-between gap-2 py-1/2 w-[20%] lg:w-[10%]'>
               <button 
-                className='flex justify-center w-full bg-blue-400 text-white font-bold rounded-full hover:scale-110 shadow-lg'
+                className='flex justify-center items-center p-1 w-full bg-blue-400 text-white font-bold rounded-full hover:scale-110 shadow-lg'
                 onClick={handleEdit}
-              >Edit</button>
+              ><i className="fa-solid fa-pen-to-square"></i></button>
               <button
-                className='flex justify-center w-full bg-red-400 text-white font-bold rounded-full hover:scale-110 shadow-lg'
+                className='flex justify-center items-center p-1 w-full bg-red-400 text-white font-bold rounded-full hover:scale-110 shadow-lg'
                 onClick={handleDelete}
-              >Delete</button>
+              ><i className="fa-solid fa-trash"></i></button>
             </div>
           </>
       }
